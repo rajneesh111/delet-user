bfad0be9c3c08e67da2a7fb14c3b980181d62056
Rajneesh Singh <102209100+rajneesh111@users.noreply.github.com>
2023-08-22 02:04:49 +0530

    Update delete-user.sh

--- delete-user.sh
+++ delete-user.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
-
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <filename>"
     exit 1
 fi
 
+# Assign the filename from the command line argument
 FILENAME=$1
 
+# Check if the file exists
 if [ ! -f "$FILENAME" ]; then
     echo "File '$FILENAME' not found."
     exit 1
@@ -15,7 +15,7 @@ fi
 
 while IFS= read -r USERNAME; do
     if id "$USERNAME" &>/dev/null; then
-        sudo userdel -r "$USERNAME"
+        sudo userdel  "$USERNAME"
         echo "User '$USERNAME' has been deleted without deleting the home directory."
     else
         echo "User '$USERNAME' does not exist."
